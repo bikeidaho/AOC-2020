95d7338ed594d1fffbcc75a9999f70d4f79d2193
bikeidaho <43679674+bikeidaho@users.noreply.github.com>
2020-12-09 16:05:26 -0700

    Create day2-prob2.py

---
+++ Day2/day2-prob2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+input = open('.\Day2\day2-input.txt')
+listofpasswords = input.read().splitlines()
+
+list = np.asarray(listofpasswords)
+initialcount = 0
+
+print("Initial Count = " +str(initialcount))
+for item in listofpasswords:
+    compare = item.split()
+    letter = compare[1].replace(':','')
+    
+    range = compare[0]
+    rangedelimited = range.replace('-', ' ')
+    rangesplit = rangedelimited.split()
+    
+    password = compare[2]
+    first = rangesplit[0]
+    second = rangesplit[1]
+   
+    print("Password " +password)
+    print("First Pos " +first)
+    print("Second Pos " +second)
+    
+    print("Searching for " +letter)
+    
+    firstpos = password[int(first)-1]
+    secondpos = password[int(second)-1]
+    print("First letter " +firstpos)
+    print("Second letter " +secondpos)
+
+    if firstpos == letter or secondpos == letter:
+        initialcount +=1
+        print("Possible")
+        print(initialcount)
+    else:
+        print("Not Possible")
